8075ed1614d8b26645623915539cbae67b4aa57f
052787 <052787@SPFAM5310F-01.famacad.com.br>
2025-03-18 21:18:37 -0300

    Projeto da aula

---
+++ src/main/java/br/com/eanassu/dao/DaoAluno.java
@@ -0,0 +1,103 @@
+package br.com.eanassu.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import br.com.eanassu.pojo.Aluno;
+
+public class DaoAluno {
+	private Connection connection;
+	private String url = "jdbc:hsqldb:hsql://localhost/";
+	private String user = "SA";
+	private String password = "";
+
+	public DaoAluno() {
+		try {
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void insert(Aluno aluno) {
+		String sql = "INSERT INTO ALUNOS VALUES(?,?,?,?)";
+		try {
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, aluno.getRa());
+			pstmt.setString(2, aluno.getNome());
+			pstmt.setDate(3, new java.sql.Date(aluno.getDataNascimento().getTime()));
+			pstmt.setDouble(4, aluno.getRenda());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void delete(Aluno aluno) {
+		String sql = "DELETE FROM ALUNOS WHERE RA = ?";
+		try {
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, aluno.getRa());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void update(Aluno aluno) {
+		String sql = "UPDATE ALUNOS SET NOME=?,DATANASCIMENTO=?,RENDA=? WHERE RA=?";
+		try {
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+			pstmt.setString(1, aluno.getNome());
+			pstmt.setDate(2, new java.sql.Date(aluno.getDataNascimento().getTime()));
+			pstmt.setDouble(3, aluno.getRenda());
+			pstmt.setInt(4, aluno.getRa());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public List<Aluno> getLista() {
+		String sql = "SELECT * FROM ALUNOS";
+		List<Aluno> lista = new ArrayList<Aluno>();
+		try {
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next()) {
+				int ra = rs.getInt(1);
+				String nome = rs.getString(2);
+				Date dataNascimento = rs.getDate(3);
+				Double renda = rs.getDouble(4);
+				lista.add(new Aluno(ra, nome, dataNascimento, renda));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return lista;
+	}
+
+	public Aluno buscarPeloRa(int ra) {
+		String sql = "SELECT * FROM ALUNOS WHERE RA=?";
+		try {
+			PreparedStatement pstmt = connection.prepareStatement(sql);
+			pstmt.setInt(1, ra);
+			ResultSet rs = pstmt.executeQuery();
+			if (rs.next()) {
+				String nome = rs.getString(2);
+				Date dataNascimento = rs.getDate(3);
+				Double renda = rs.getDouble(4);
+				return new Aluno(ra, nome, dataNascimento, renda);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
