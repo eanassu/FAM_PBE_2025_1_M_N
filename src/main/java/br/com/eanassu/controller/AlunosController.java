9d672ecb3c6a53e4e711ac29d1be93913b880677
052787 <052787@SPFAM5310F-01.famacad.com.br>
2025-03-25 20:35:57 -0300

    aula 25/3

---
+++ src/main/java/br/com/eanassu/controller/AlunosController.java
@@ -0,0 +1,41 @@
+package br.com.eanassu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.eanassu.dao.DaoAluno;
+import br.com.eanassu.pojo.Aluno;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/alunos")
+public class AlunosController {
+
+	private DaoAluno dao = new DaoAluno();
+
+	@GetMapping
+	public String showAlunos() {
+		return "alunos";
+	}
+
+	@GetMapping("/")
+	public String showAlunosBarra() {
+		return "alunos";
+	}
+
+	@GetMapping("/new")
+	public String showInsertForm(Model model) {
+		model.addAttribute("aluno", new Aluno());
+		return "create-aluno";
+	}
+	@PostMapping
+	public String createAluno(@ModelAttribute Aluno aluno) {
+		dao.insert(aluno);
+		return "redirect:alunos";
+	}
+}
