d4777554ba0e896e060ed15088cbf3be5daf5f77
052787 <052787@SPFAM5310F-01.famacad.com.br>
2025-04-29 20:02:54 -0300

    UserDetails no banco de dados

--- src/main/java/br/com/eanassu/SecurityConfig.java
+++ src/main/java/br/com/eanassu/SecurityConfig.java
@@ -1,5 +1,8 @@
 package br.com.eanassu;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,27 +10,53 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	@Autowired
+	private DataSource dataSource;
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
-	@Bean
-	public UserDetailsService userDetailsService() {
-		UserDetails user = User.builder()
-				.username("usuario")
-				.password(passwordEncoder().encode("senha"))
-				.roles("USER")
-				.build();
-		return new InMemoryUserDetailsManager(user);
+	private boolean userExists(JdbcUserDetailsManager users, String username) {
+		try {
+			users.loadUserByUsername(username);
+			return true;
+		} catch(UsernameNotFoundException e) {
+			return false;
+		}
 	}
+    @Bean
+    public JdbcUserDetailsManager users(PasswordEncoder encoder) {
+    	JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+    	String sql1 = "SELECT username,password,enabled from users where username=?";
+    	String sql2 = "SELECT username,authority from authorities where username=?";
+    	users.setUsersByUsernameQuery(sql1);
+    	users.setAuthoritiesByUsernameQuery(sql2);
+    	if ( !userExists(users,"admin")) {
+    		UserDetails admin=User.builder().username("admin")
+    				.password(encoder.encode("admin123"))
+    				.roles("ADMIN","USER").build();
+    		users.createUser(admin);
+    	}
+    	if ( !userExists(users,"user")) {
+    		UserDetails user=User.builder().username("user")
+    				.password(encoder.encode("user123"))
+    				.roles("USER").build();
+    		users.createUser(user);
+    	}
+    	return users;
+    }
+
 
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(auth->
